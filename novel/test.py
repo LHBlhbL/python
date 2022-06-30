8ad694c49024d9b424c178c1388b4ccc454fe6c3
Fivefive <2712544015@qq.com>
2022-06-30 15:50:31 +0800

    modify

--- novel/test.py
+++ novel/test.py
@@ -12,25 +12,30 @@ headers = {
 }
 
 
-def novel_request(url,preText):
+def novel_request(url,preText,fileName):
     newUrl=url
     novel_text=preText
+    pre='下一章'
     while newUrl != "":
         html = requests.get(newUrl, headers=headers).content.decode('utf-8')
         soup = BeautifulSoup(html, 'html.parser')
         list1 = soup.find(id='booktxt')
         list2 = list1.find_all('p')
+        if pre == '下一章':
+            novel_text+=soup.find('h1').text+'\n'
         for item in list2:
             novel_text += item.text+'\n'
         newItem=soup.find(id='next_url')
         new=newItem.get('href')
         newUrl = "https://www.husttest.com"+new
         if newItem.text == '下一页':
+            pre='下一页'
             continue
         elif newItem.text == '下一章':
-            with open('novel3.txt', 'a', encoding='utf-8')as f:
+            with open(fileName, 'a', encoding='utf-8')as f:
                 f.write(novel_text)
             novel_text=""
+            pre='下一章'
         else:
             newUrl=""
 
@@ -39,4 +44,5 @@ def novel_request(url,preText):
 if __name__ == "__main__":
     pre=""
     url=input("url:")
-    novel_request(url,pre)
+    fileName=input("fileName:")
+    novel_request(url,pre,fileName)
